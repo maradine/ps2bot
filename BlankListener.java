4765bfd3c38e70cf94183b9fbe0f255b83c33af6
maradine <maradine@gmail.com>
2013-02-27 05:56:32 +0000

    Removal of cruft

--- BlankListener.java
+++
@@ -1,9 +0,0 @@
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.MessageEvent;
-
-public class BlankListener extends ListenerAdapter {
-
-	public void onMessage(MessageEvent event) {
-		event.respond("BORK");
-	}
-}
