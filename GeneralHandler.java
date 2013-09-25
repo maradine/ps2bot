6c8eed6dcaa465156eebfd8b87ad688fb5f2d96f
maradine <maradine@gmail.com>
2013-09-25 17:57:23 +0000

    now with granular shutdown for multi-bot environments

--- GeneralHandler.java
+++ GeneralHandler.java
@@ -22,21 +22,34 @@ public class GeneralHandler extends ListenerAdapter {
 			
 	public void onMessage(MessageEvent event) {
 
-		String rawcommand = event.getMessage();
-		String command = rawcommand.toLowerCase();
+		String command = event.getMessage();
+		String commandLower = command.toLowerCase();
 		
-		
-		if (command.equals("!shutdown")){  
+		scanner = new Scanner(commandLower);
+		String token = scanner.next();
+
+		if (token.equals("!shutdown")) {
+
 			if (!pm.isAllowed(command,event.getUser(),event.getChannel())) {
 				event.respond("Sorry, you do not have permission to execute this command.");
 				return;
+			} else {
+				if (scanner.hasNext()) {
+					token = scanner.next();
+
+					String myName = event.getBot().getNick().toLowerCase();
+					if (token.equals(myName)) {
+						event.respond("Nite nite.");
+						bot.shutdown(true);
+						try {
+							Thread.sleep(5000L);
+						} catch (Exception e) {}
+						System.exit(0);
+					}
+				} else {
+					event.respond("You need to call me by name.  It's crowded in here.");
+				}
 			}
-			event.respond("Nite nite.");
-			bot.shutdown(true);
-			try {
-				Thread.sleep(5000L);
-			} catch (Exception e) {}
-			System.exit(0);
 		}
 
 	}
